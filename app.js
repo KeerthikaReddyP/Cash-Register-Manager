2311cc9d45939c4a8e1c5028936cb21dfac2ed9c
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-04-17 10:29:56 +0530

    Updated logic of the program

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
-const billAmount=document.querySelector("#bill");
-const cashGiven=document.querySelector("#cash");
-const nextBtn=document.querySelector("#next-btn");
-const checkBtn=document.querySelector("#check-btn");
-const billErrMsg=document.getElementById("bill-validator");
+const billAmnt = document.querySelector("#bill");
+const cashGiven = document.querySelector("#cash");
+
+const nxtBtn = document.querySelector("#next-btn");
+const checkBtn = document.querySelector("#check-btn");
+
+const billErrMsg = document.getElementById("bill-validator");
+const cashErrMsg = document.getElementById("cash-validator");
+
+const afterBill = document.getElementById("afterbill");
+const afterCash = document.getElementById("aftercash");
+
 var tableArr=document.querySelectorAll(".table-data");
 
 const notesArr=[2000,500,100,20,10,5,1];
 
-document.getElementById("afterbill").style.display="none";
-document.getElementById("aftercash").style.display="none";
 
-function nextBtnHandler(){
-    if(billAmount.value>0){
-        billErrMsg.style.display="none";
-        document.getElementById("afterbill").style.display="block";
-        
-    }else{
-        billErrMsg.style.display="block";
-    }
+function toggle(elem, displayy) {
+  elem.style.display = displayy;
+}
+toggle(billErrMsg, "none");
+toggle(cashErrMsg, "none");
+toggle(afterBill, "none");
+toggle(afterCash, "none");
+
+
+function calcNotes(amount) {
+  for (var i = 0; i < notesArr.length; i++) {
+    tableArr[i].innerText = Math.trunc(amount / notesArr[i]);
+    amount = amount % notesArr[i];
+  }
 }
 
-function checkBtnHandler(){
-    if(billAmount.value<=cashGiven.value){
-        document.getElementById("aftercash").style.display="block";
-        calcNotes(cashGiven.value-billAmount.value);
-    }else{
-        document.getElementById("cash-validator").style.display="block";
-    }
+
+function nextBtnHandler() {
+  if (billAmnt.value <= 0) {
+    toggle(billErrMsg, "block");
+    toggle(afterBill, "none");
+  } else {
+    toggle(afterBill, "block");
+    toggle(billErrMsg, "none");
+  }
 }
 
-function calcNotes(amount){
-    for(var i=0;i<notesArr.length;i++){
-        tableArr[i].innerText=Math.trunc(amount/notesArr[i]);
-        amount=amount%notesArr[i];
-    }
+function checkBtnHandler() {
+  if (Number(billAmnt.value) <= Number(cashGiven.value)) {
+    toggle(afterCash, "block");
+    toggle(cashErrMsg, "none");
+    calcNotes(cashGiven.value - billAmnt.value);
+  } else {
+    toggle(cashErrMsg, "block");
+    toggle(afterCash, "none");
+  }
 }
 
-nextBtn.addEventListener("click", nextBtnHandler);
-checkBtn.addEventListener("click",checkBtnHandler);
+nxtBtn.addEventListener("click", nextBtnHandler);
+checkBtn.addEventListener("click", checkBtnHandler);
+
